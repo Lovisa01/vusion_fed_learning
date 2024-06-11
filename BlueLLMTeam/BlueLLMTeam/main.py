56c231078b5d5f11cce1d333632a85cbcb61e8fa
Frank Parker <franklin.parker.kubota@gmail.com>
2024-06-11 04:18:46 -0500

    Updated the folder structure

--- BlueLLMTeam/BlueLLMTeam/main.py
+++ BlueLLMTeam/BlueLLMTeam/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         "systemRole": "You're a linux terminal that needs to provide ",
         "user": "Linux developer",
         "context": "Files should be based on bread bakery",
-        "message": "can you create the code in linux to create a folder structure for a bakery. Guess what that should look like. Task: provide a json of the code with the files. JSON: [{'Content_Type':'code  for creating File,folder,package etc...','Contents':' code if file then it should be file contents, if folder should be folder name'},{'Content_Type':'File,folder,package etc...','Contents':'if file then it should be file contents, if folder should be folder name'}]",
+        "message": "can you create the code to create a folder structure for a bakery. Guess what that should look like. Task: provide a json of the code with the files. JSON: [{'Content_Type':'code  for creating File,folder,package etc...','Contents':' code if file then it should be file contents, if folder should be folder name'},{'Content_Type':'File,folder,package etc...','Contents':'if file then it should be file contents, if folder should be folder name'}]",
         "model" : "gpt-3.5-turbo"
     }
     
