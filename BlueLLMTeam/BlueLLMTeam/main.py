3bff62ef354129a46d5f7e5f13e6cd6ae8512e80
Frank Parker <franklin.parker.kubota@gmail.com>
2024-06-11 08:26:34 -0500

    Fixed the GPT typo from get to GPT

--- BlueLLMTeam/BlueLLMTeam/main.py
+++ BlueLLMTeam/BlueLLMTeam/main.py
@@ -5,10 +5,9 @@ if __name__ == "__main__":
     prompt_dict = {
         "systemRole": "You're a linux terminal that needs to provide ",
         "user": "Linux developer",
-        "context": "Files should be based on bread bakery",
+        "context": "Company Name, Industry, Service or Products, Employee Size, Customer Size. I.E. Panera Bread, Bread Franchise Restraunt, Service and Products",
         "message": "can you create the code to create a folder structure for a bakery. Guess what that should look like. Task: provide a json of the code with the files. JSON: [{'Content_Type':'code  for creating File,folder,package etc...','Contents':' code if file then it should be file contents, if folder should be folder name'},{'Content_Type':'File,folder,package etc...','Contents':'if file then it should be file contents, if folder should be folder name'}]",
         "model" : "gpt-3.5-turbo",
-        "response_format" : "json_object"
     }
     
     # Create an instance of ChatGPTEndpoint
